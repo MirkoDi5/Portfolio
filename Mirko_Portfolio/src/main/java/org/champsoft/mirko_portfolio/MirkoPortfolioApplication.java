2fa269c43eb10ebbe45902d6274761fd39c71951
MirkoDi5 <mirtheboss10@outlook.com>
2026-01-20 01:32:09 -0500

    Starting the portfolio

---
+++ Mirko_Portfolio/src/main/java/org/champsoft/mirko_portfolio/MirkoPortfolioApplication.java
@@ -0,0 +1,13 @@
+package org.champsoft.mirko_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirkoPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MirkoPortfolioApplication.class, args);
+    }
+
+}
---
+++ Mirko_Portfolio/src/test/java/org/champsoft/mirko_portfolio/MirkoPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.champsoft.mirko_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirkoPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
